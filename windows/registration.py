f21591853558640f481d14cbbdd49b88264124ca
FrozZzy1 <frozzzy71@gmail.com>
2023-10-15 05:37:26 +0700

    update(windows): delete unnecessary import

--- windows/registration.py
+++ windows/registration.py
@@ -1,12 +1,6 @@
-from PyQt6.QtWidgets import QLabel, QLineEdit
-
 from windows.template import TemplateWindow
 
 
 class RegistrationWindow(TemplateWindow):
     def __init__(self):
         super().__init__()
-
-
-
-
