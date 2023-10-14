e64b3c81916730ea23ab6a51ccd6c9fe108667aa
FrozZzy1 <frozzzy71@gmail.com>
2023-10-15 03:19:33 +0700

    add(windows): add registration and authorization windows

---
+++ windows/authorization.py
@@ -0,0 +1,12 @@
+from windows.template import TemplateWindow
+
+
+class AuthorizationWindow(TemplateWindow):
+    def __init__(self):
+        super().__init__()
+
+#        self.setStyleSheet("""
+#                QWindow {
+#               background-image: url(images/background__purple.png);
+#               }
+#               """)
